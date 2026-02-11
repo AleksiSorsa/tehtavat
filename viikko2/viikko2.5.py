d3ceeb8ceabce56ff789215a0e51d6bce2bf8259
Aleksi <sorsa.aleksi01@gmail.com>
2026-02-11 13:53:46 +0200

    test

--- viikko2/viikko2.5.py
+++ viikko2/viikko2.5.py
@@ -4,12 +4,12 @@ luodit = float(input("Anna luodit: "))
 
 #muunnokset :
 luoti_grammoina = 13.3
-naula_grammoina = 32 * luoti_grammoina
-leiviskat_grammoina = 20 * naula_grammoina
+naulat_grammoina = 32 * luoti_grammoina
+leiviskat_grammoina = 20 * naulat_grammoina
 
 massa_grammoina = (leiviskat * leiviskat_grammoina + naulat * naulat_grammoina + luodit * luoti_grammoina)
 
 kilot = int(massa_grammoina // 1000)
-grammat = massa_grammoina & 1000
+grammat = massa_grammoina % 1000
 
 print(f"{kilot} kilogrammaa ja {grammat} grammaa.")
